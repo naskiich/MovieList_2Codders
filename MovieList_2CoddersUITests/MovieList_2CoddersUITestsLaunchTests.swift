4451378bc4445fba258fa803deb2f98fdedd742a
naskiich <naskovam@yahoo.com>
2022-03-17 13:17:34 +0100

    Initial Commit

---
+++ MovieList_2CoddersUITests/MovieList_2CoddersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieList_2CoddersUITestsLaunchTests.swift
+//  MovieList_2CoddersUITests
+//
+//  Created by Marija Naskova on 17.3.22.
+//
+
+import XCTest
+
+class MovieList_2CoddersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
